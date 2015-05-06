c1b7258d695fa9223678bb753e7e8515c06e10b6
wenghe <aw_wenghe@163.com>
2015-05-06 15:52:31 +0800

    添加首页

--- application/views/front/default/index.php
+++ application/views/front/default/index.php
@@ -3,7 +3,7 @@
 <body>
 <div class="header">
     <div class="wrap">
-        <a href="/" alt="掌易科技" class="l logo" style="background:url(/static/logo/logo_site.png) no-repeat;width:300px;height:70px;"></a>
+        <a href="/" alt="掌易科技" class="l logo" style="background:url(/style/front/sty_default/logo/logo_site.png) no-repeat;width:300px;height:70px;"></a>
         <div class="r">
             <div class="l nav">
                 <ul>
@@ -12,7 +12,7 @@
                     <?php foreach($cate as $k => $v){?>
                     <li><a  href="<?php echo $v['surl'];?>"><?php echo $v['cname'];?></a></li>
                     <?php }?>
-                    <li><a  href="/index.php?tpl=list&cid=4&p=1">关于我们</a></li><li><a  href="/index.php?tpl=list&cid=11&p=1">商业服务</a></li><li><a  href="/app/message/">给我留言</a></li>                
+                    <li><a  href="/index.php?tpl=list&cid=4&p=1">关于我们</a></li><li><a  href="/app/message/">给我留言</a></li>                
                 </ul>
             </div>
         </div>
@@ -33,27 +33,32 @@
 </div>
 
     <div class="silder" id="silder">
-        <script type="text/javascript" src="/static/libs/img.silder/jquery.img.silder.big.js"></script>
+        <script type="text/javascript" src="/style/libs/img.silder/jquery.img.silder.js"></script>
         <script>
             $(function(){
-                $('#silder').imgSilderBig({s_width:'100%', s_height:528, on:'mouseover', is_showTit:false, s_times:3000,css_link:'/static/libs/img.silder/style.css'});
+                $('#silder').imgSilder({s_width:'100%', s_height:528, on:'mouseover', is_showTit:false, s_times:3000,css_link:'/style/libs/img.silder/style.css'});
             });
         </script>
         <ul class="silder_list" id="silder_list">
-            <li style="background:url(/static/logo/index1.jpg) center center no-repeat;"><a href="" style="display:block;text-indent:-2000px;width:500px;height:100px;margin:200px auto 0 auto;" target="_blank"></a></li><li style="background:url(/static/logo/index2.jpg) center center no-repeat;"><a href="" style="display:block;text-indent:-2000px;width:500px;height:100px;margin:200px auto 0 auto;" target="_blank"></a></li>        </ul>
+            <li style="background:url(/style/front/sty_default/logo/index1.jpg) center no-repeat;"><a href=""  target="_blank"></a></li>
+            <li style="background:url(/style/front/sty_default/logo/index2.jpg) center no-repeat;"><a href="" target="_blank"></a></li>       
+        </ul>
     </div>
 
     <div class="imglist1">
         <div class="line_10"></div>
 
         <ul class="wrap clearfix">
-            <li><a href="http://crane/index.php?tpl=content&id=14" style="background:url(/static/logo/preview/tedian1.jpg) no-repeat;">服务端响应式设计</a></li><li><a href="http://crane/index.php?tpl=content&id=15" style="background:url(/static/logo/preview/tedian2.jpg) no-repeat;">集成微网站接口</a></li><li><a href="http://crane/index.php?tpl=content&id=16" style="background:url(/static/logo/preview/tedian3.jpg) no-repeat;">安全可靠的系统结构</a></li>        </ul>
+            <li><a href="http://crane/index.php?tpl=content&id=14" style="background:url(/style/front/sty_default/logo/preview/tedian1.jpg) no-repeat;">服务端响应式设计</a></li>
+            <li><a href="http://crane/index.php?tpl=content&id=15" style="background:url(/style/front/sty_default/logo/preview/tedian2.jpg) no-repeat;">集成微网站接口</a></li>
+            <li><a href="http://crane/index.php?tpl=content&id=16" style="background:url(/style/front/sty_default/logo/preview/tedian3.jpg) no-repeat;">安全可靠的系统结构</a></li>        
+        </ul>
     </div>
     <div class="footer">
     <div class="wrap service clearfix">
         <div class="clearfix" style="height:30px;"></div>
         <div class="l" style="width:220px;">
-            <img src="/static/default/qrcode/qrcode_5.png">
+             <img src="/style/front/sty_default/qrcode/qrcode_5.png"> 
         </div>
         <ul class="l">
             <li class="top">商业服务</li><li><a href="http://crane/index.php?tpl=content&id=1">定制开发</a></li><li><a href="http://crane/index.php?tpl=content&id=2">技术支持</a></li><li><a href="http://crane/index.php?tpl=content&id=3">短信通知</a></li>        </ul>
