d6847f811753f5b0e86b082536505633845bb9d8
wenghe <aw_wenghe@163.com>
2015-07-05 14:07:48 +0800

    添加图片处理

---
+++ application/controllers/init/install.php
@@ -0,0 +1,16 @@
+<?php 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * 安装系统
+ */
+class Install extends CBase {
+    
+    public function __construct() {
+        
+        parent::__construct();
+    }
+    
+    public function index(){
+        
+    }
+}
