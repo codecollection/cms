2da418570b4063ecf1b8e3eb7bc50091377955ad
wenghe <aw_wenghe@163.com>
2015-08-01 17:04:38 +0800

    fix/

--- application/views/front/public/cover.php
+++ application/views/front/public/cover.php
@@ -1263,20 +1263,7 @@
 <!--排行榜 end-->
 </div>
 <!--登录弹出框 b-->
-<div class="adapt-cont" id="baseLog">
-    <h2>
-        <a href="javascript:void(0)" id="baseLogC" title="关闭">
-            <span></span>
-        </a>
-        <i></i>
-        登录
-    </h2>
-    <p class="adapt-success adapt-success-special"><img src="/style/front/public/bear.jpg" o-src="http://raw.android.d.cn/cdroid_res/web/news2015061516/img/bear.jpg" alt="" />登录后才能喜欢哦！</p>
-    <a href="" title="立即登录" class="log-now">立即登录</a>
-</div>
-<!--登录弹出框 e-->
-<div class="overlay" id="overlay"></div>
-<a id="toTop" title="返回顶部" target="_self" href="javascript:void(0)"><i></i></a>
+<?php $c->loadView("front/public/login.nav.php");?>
 
 <div class="ft">
     <div class="qq-t">
